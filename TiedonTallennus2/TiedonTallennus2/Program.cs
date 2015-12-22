fcab0eefb6398aa80cbfd864875fb9a86008a30b
Pauli Sutelainen <pauli.sutelainen@gmail.com>
2015-12-22 15:21:30 +0200

    Tiedostonkäsittely lisätty.

--- TiedonTallennus2/TiedonTallennus2/Program.cs
+++ TiedonTallennus2/TiedonTallennus2/Program.cs
@@ -14,13 +14,21 @@ namespace TiedonTallennus2
     {
         static void Main(string[] args)
         {
+            string tiedosto = @"C:\temp\lämpötila.txt";
+            // luetaan tiedostosta aiemmin asetettu lämpötila
+            if (File.Exists(tiedosto))
+            {
+                string aiempiArvo = ReadAllText(tiedosto);
+                Console.WriteLine("Aiemmin asetettu arvo: " + aiempiArvo);
+
+            }
             int lämpötila = 0;
             Console.Write("Anna uusi lämpötila: ");
             string syöte = Console.ReadLine();
             lämpötila = int.Parse(syöte);
 
             // tiedostoon kirjoittaminen
-            string tiedosto = @"C:\temp\lämpötila.txt";
+
             File.WriteAllText(tiedosto, lämpötila.ToString());
             Console.WriteLine("Uusi lämpötila tallennettu.");
             Console.ReadLine();
