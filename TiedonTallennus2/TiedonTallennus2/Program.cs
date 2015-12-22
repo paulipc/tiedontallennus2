875d3c52b1bd95f9638777bdef65b6753d905db8
Pauli Sutelainen <pauli.sutelainen@gmail.com>
2015-12-22 14:49:36 +0200

    v1.1

--- TiedonTallennus2/TiedonTallennus2/Program.cs
+++ TiedonTallennus2/TiedonTallennus2/Program.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.IO.File;
+
+
 
 namespace TiedonTallennus2
 {
@@ -10,6 +14,18 @@ namespace TiedonTallennus2
     {
         static void Main(string[] args)
         {
+            int lämpötila = 0;
+            Console.Write("Anna uusi lämpötila: ");
+            string syöte = Console.ReadLine();
+            lämpötila = int.Parse(syöte);
+
+            // tiedostoon kirjoittaminen
+            string tiedosto = @"C:\temp\lämpötila.txt";
+            File.WriteAllText(tiedosto, lämpötila.ToString());
+            Console.WriteLine("Uusi lämpötila tallennettu.");
+            Console.ReadLine();
+
+
         }
     }
 }
